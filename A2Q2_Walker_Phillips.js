a5e91f87f4112621754742c71ddf7a00c5fb26dd
walkerphillips04 <124199383+walkerphillips04@users.noreply.github.com>
2023-03-01 17:36:34 -0500

    Update A2Q2_Walker_Phillips.js

--- A2Q2_Walker_Phillips.js
+++ A2Q2_Walker_Phillips.js
@@ -4,13 +4,20 @@ function setup() {
 
 function draw() {
   background(220);
-  
-  fill("red")
-  angleMode(DEGREES)
-  noStroke()
-  rect(140,100,130)
- rotate(45)
-  rect(190,-100,140)
+fill("red")
+  push(); 
+  translate(width / 2, height / 2);
+  beginShape();
+  for (let i = 0; i < 8; i++) {
+    let angle = i * TWO_PI / 8; 
+    let x = sin(angle) * 100; 
+    let y = cos(angle) * 100; 
+    vertex(x, y); 
+    angle += TWO_PI / 16; 
+    x = sin(angle) * 50; 
+    y = cos(angle) * 50; 
+    vertex(x, y); 
+  }
+  endShape(CLOSE); 
+  pop(); 
 }
-
-
