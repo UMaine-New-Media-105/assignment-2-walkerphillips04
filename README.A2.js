78d282da688ad55e641993c5dbe239a68675542f
walkerphillips04 <124199383+walkerphillips04@users.noreply.github.com>
2023-03-01 17:39:56 -0500

    Create README.A2.js

---
+++ README.A2.js
@@ -0,0 +1,6 @@
+# assignment-2-walkerphillips04
+assignment-2-walkerphillips04 created by GitHub Classroom
+For A2Q1: https://editor.p5js.org/walker.phillips/sketches/xnjmSLIfS
+For A2Q2: https://editor.p5js.org/walker.phillips/sketches/FihEPOezz
+For A2Q3: https://editor.p5js.org/walker.phillips/sketches/iH7n9T8DH
+For A2Q4: https://editor.p5js.org/walker.phillips/sketches/4zjyk4_KO
